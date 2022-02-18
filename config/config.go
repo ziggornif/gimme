33d64c118aac96ef01041f48b5c5663c9ac2ab3d
ziggornif <matthieudrouian@gmail.com>
2022-02-18 22:06:04 +0100

    feat: end of config improvements

--- config/config.go
+++ config/config.go
@@ -9,50 +9,43 @@ import (
 )
 
 type Configuration struct {
-	AdminUser     string `json:"adminUser" mapstructure:"GIMME_ADMIN_USER"`
-	AdminPassword string `json:"adminPassword" mapstructure:"GIMME_ADMIN_PASSWORD"`
-	Secret        string `json:"secret" mapstructure:"GIMME_SECRET"`
-	S3Url         string `json:"S3Url" mapstructure:"GIMME_SECRET"`
-	S3Key         string `json:"S3Key" mapstructure:"GIMME_S3_URL"`
-	S3Secret      string `json:"s3Secret" mapstructure:"GIMME_S3_SECRET"`
-	S3BucketName  string `json:"s3BucketName" mapstructure:"GIMME_S3_BUCKET_NAME"`
-	S3Location    string `json:"s3Location" mapstructure:"GIMME_S3_LOCATION"`
-	S3SSL         bool   `json:"s3SSL" mapstructure:"GIMME_S3_SSL"`
+	AdminUser     string
+	AdminPassword string
+	Secret        string
+	S3Url         string
+	S3Key         string
+	S3Secret      string
+	S3BucketName  string
+	S3Location    string
+	S3SSL         bool
 }
 
 func NewConfig() (*Configuration, error) {
 	viper.SetConfigName("gimme")
-	viper.SetConfigType("json")
-	viper.AddConfigPath(".")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")       // local path
+	viper.AddConfigPath("/config") // docker path
 	viper.AutomaticEnv()
 
 	viper.SetDefault("s3.bucketName", "gimme")
 	viper.SetDefault("s3.ssl", true)
 
 	err := viper.ReadInConfig()
-	//if err != nil {
-	//	logrus.Errorf("Unable to read the config file: %s", err)
-	//	return nil, err
-	//}
+	if err != nil {
+		logrus.Errorf("Unable to read the config file: %s", err)
+		return nil, err
+	}
 
 	config := Configuration{}
-
-	if err != nil {
-		err = viper.Unmarshal(&config)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		config.AdminUser = viper.GetString("admin.user")
-		config.AdminPassword = viper.GetString("admin.password")
-		config.Secret = viper.GetString("secret")
-		config.S3Url = viper.GetString("s3.url")
-		config.S3Key = viper.GetString("s3.key")
-		config.S3Secret = viper.GetString("s3.secret")
-		config.S3BucketName = viper.GetString("s3.bucketName")
-		config.S3Location = viper.GetString("s3.location")
-		config.S3SSL = viper.GetBool("s3.ssl")
-	}
+	config.AdminUser = viper.GetString("admin.user")
+	config.AdminPassword = viper.GetString("admin.password")
+	config.Secret = viper.GetString("secret")
+	config.S3Url = viper.GetString("s3.url")
+	config.S3Key = viper.GetString("s3.key")
+	config.S3Secret = viper.GetString("s3.secret")
+	config.S3BucketName = viper.GetString("s3.bucketName")
+	config.S3Location = viper.GetString("s3.location")
+	config.S3SSL = viper.GetBool("s3.ssl")
 
 	err = validateConfig(&config)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -37,18 +37,18 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/rs/xid v1.2.1 // indirect
-	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/afero v1.8.1 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/crypto v0.0.0-20211108221036-ceb1ce70b4fa // indirect
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
-	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/ini.v1 v1.66.2 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
