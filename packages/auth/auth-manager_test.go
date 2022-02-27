fe0bd23ccb5f81ba82116f996214c99a1098eb20
ziggornif <matthieudrouian@gmail.com>
2022-02-27 19:42:05 +0100

    test: improve auth coverage

--- packages/auth/auth-manager_test.go
+++ packages/auth/auth-manager_test.go
@@ -1,14 +1,34 @@
 package auth
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
 	"time"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/stretchr/testify/assert"
 )
 
 var jwtRegex = `^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$`
+
+type header struct {
+	Key   string
+	Value string
+}
+
+func performRequest(r http.Handler, method, path string, headers ...header) *httptest.ResponseRecorder {
+	req := httptest.NewRequest(method, path, nil)
+	for _, h := range headers {
+		req.Header.Add(h.Key, h.Value)
+	}
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
 
 func TestCreateTokenError(t *testing.T) {
 	authManager := NewAuthManager("secret")
@@ -34,4 +54,60 @@ func TestCreateTokenCustomExp(t *testing.T) {
 
 	assert.Regexp(t, regexp.MustCompile(jwtRegex), token)
 	assert.Nil(t, err)
+}
+
+func TestAuthManager_AuthenticateMiddlewareErr(t *testing.T) {
+	authManager := NewAuthManager("secret")
+	router := gin.New()
+	router.GET("/", authManager.AuthenticateMiddleware, func(c *gin.Context) {
+	})
+
+	w := performRequest(router, "GET", "/")
+
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+}
+
+func TestAuthManager_AuthenticateMiddlewareInvalid(t *testing.T) {
+	authManager := NewAuthManager("secret")
+	router := gin.New()
+	router.GET("/", authManager.AuthenticateMiddleware, func(c *gin.Context) {
+	})
+
+	w := performRequest(router, "GET", "/", header{Key: "Authorization", Value: "invalid"})
+
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+}
+
+func TestAuthManager_AuthenticateMiddlewareInvalid2(t *testing.T) {
+	authManager := NewAuthManager("secret")
+	router := gin.New()
+	router.GET("/", authManager.AuthenticateMiddleware, func(c *gin.Context) {
+	})
+
+	w := performRequest(router, "GET", "/", header{Key: "Authorization", Value: "Bearer invalid"})
+
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+}
+
+func TestAuthManager_AuthenticateMiddlewareExpired(t *testing.T) {
+	authManager := NewAuthManager("secret")
+	router := gin.New()
+	router.GET("/", authManager.AuthenticateMiddleware, func(c *gin.Context) {
+	})
+
+	w := performRequest(router, "GET", "/", header{Key: "Authorization", Value: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDU5ODcwNTIsImp0aSI6InppZyJ9.q9NbUVV6egGlZBLMbvRBO_-VnWy_edDT4VNU6g8GIxQ"})
+
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+}
+
+func TestAuthManager_AuthenticateMiddlewareOK(t *testing.T) {
+	authManager := NewAuthManager("secret")
+	token, _ := authManager.CreateToken("test", "")
+	router := gin.New()
+	router.GET("/", authManager.AuthenticateMiddleware, func(c *gin.Context) {
+	})
+
+	w := performRequest(router, "GET", "/", header{Key: "Authorization", Value: fmt.Sprintf("Bearer %s", token)})
+
+	assert.Equal(t, http.StatusOK, w.Code)
 }
